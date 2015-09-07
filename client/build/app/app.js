297babf9ea0fe0f733db019ff99de86d47e4dd78
simplesthing <i.am.simplesthing@gmail.com>
2015-09-06 17:26:42 -0700

    push to server

---
+++ client/build/app/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+(function (window, angular, undefined) {
+
+  angular.module('simplesthing', ['ngLodash', 'ui.bootstrap', 'ui.router', 'ngSanitize', 'cgBusy']).config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.when('', '/home');
+
+    $stateProvider.state('home', {
+      url: '/home',
+      views: {
+        'main': {
+          templateUrl: 'app/views/home/home.html'
+        }
+      }
+    }).state('style', {
+      url: '/style',
+      views: {
+        'main': {
+          templateUrl: 'app/views/style-guide/style-guide.html'
+        }
+      }
+    });
+  }]).value('cgBusyDefaults', {
+    templateUrl: 'app/components/cg-busy/cg-busy-template.html',
+    backdrop: false,
+    minDuration: 250
+  });
+})(window, window.angular);
