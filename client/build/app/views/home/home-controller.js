ae92a70bbddac2a83292cadd8a835f4bb1350adc
simplesthing <i.am.simplesthing@gmail.com>
2015-09-06 18:20:52 -0700

    adding renewal message

--- client/build/app/views/home/home-controller.js
+++ client/build/app/views/home/home-controller.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict';
+(function (window, angular, undefined) {
+  function HomeController() {}
+
+  HomeController.$inject = [];
+
+  angular.module('simplesthing').controller('HomeController', HomeController);
+})(window, window.angular);
---
+++ client/src/app/views/home/home-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+(function(window, angular, undefined) {
+  function HomeController(){
+
+  }
+
+  HomeController.$inject = [];
+
+  angular.module('simplesthing')
+  .controller('HomeController', HomeController);
+})(window, window.angular);
