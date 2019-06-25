be235c612556be811fb159b29106e6c736f51b55
simplesthing <i.am.simplesthing@gmail.com>
2019-06-25 11:27:00 -0700

    initial setuup

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
+
+app.get('/', (req,res) => {
+    res.send('yep')
+})
