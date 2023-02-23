349e69bf4ea45e4d7b24b509dadf18cec7b27ea4
Ava Collins <iamsimplesthing@gmail.com>
2023-02-23 15:23:04 -0800

    feat: ready player me character creator

--- pages/play/rpm.js
+++ pages/play/rpm.js
@@ -1,14 +1,14 @@
-import { Avatar, Exhibit } from '@readyplayerme/visage';
 import React, { useEffect, useRef, useState } from "react";
 
+import { Avatar } from "@readyplayerme/visage";
 import BackToHome from "../../components/back-to-home";
 import Head from "next/head";
 import Layout from "../../components/layout";
 import styled from "styled-components";
 
 const Iframe = styled.iframe`
-width: 1080px;
-height: 800px;
+  width: 1080px;
+  height: 800px;
   margin: 0;
   padding: 20px;
   font-size: 14px;
@@ -16,42 +16,43 @@ height: 800px;
 `;
 
 const Model = styled.div`
-width: 100%;
-height: 500px;
-`
+  width: 100%;
+  height: 500px;
+`;
 
 const DEFAULT_MODEL =
     "https://models.readyplayer.me/63f6e2d33cade24b03398d41.glb";
 
 const Rpm = () => {
-    const [modelUrl, setModelUrl] = useState(DEFAULT_MODEL);
+    const [modelUrl] = useState(DEFAULT_MODEL);
     const rpmRef = useRef();
 
-    useEffect(() => {
-
-    }, [])
+    useEffect(() => { }, []);
 
     return (
         <Layout>
             <Head>
                 <title>ready player me | simplesthing</title>
             </Head>
-            {/* <BackToHome anchor="#rpm" /> */}
+            <BackToHome anchor="#rpm" />
             <h1>Ready Player Me</h1>
             <section>
                 <p>
-                    Ready Player Me is a cross-game avatar platform for Unity, Unreal Engine, and web. This means you can
-                    take your avatar with you into different games and applications that support Ready Player Me SDK and not have
-                    to worry about creating a new character each time.
+                    Ready Player Me is a cross-game avatar platform for Unity, Unreal
+                    Engine, and web. They provide you with a character creator,
+                    to make your own avatar, and also host your assets at a unique url,
+                    this means you can take your avatar with you into
+                    different games and applications that support Ready Player Me SDK and
+                    not have to worry about creating a new character each time.
                 </p>
                 <Model>
                     <Avatar modelSrc={modelUrl} cameraInitialDistance={4} />
-                    {/* <Exhibit scale={1} position={[0, 0, 4]} modelSrc={modelUrl} /> */}
                 </Model>
             </section>
             <section>
                 <p>
-                    Below is a trivial example of how to implement a character creator for your own game/app using the Ready Player Me Character Creator.
+                    Below is a trivial example of how to implement a character creator for
+                    your own game/app using the Ready Player Me Character Creator.
                 </p>
             </section>
             <Iframe
@@ -61,7 +62,7 @@ const Rpm = () => {
                 ref={rpmRef}
                 on
             ></Iframe>
-            {/* <BackToHome anchor="#rpm" /> */}
+            <BackToHome anchor="#rpm" />
         </Layout>
     );
 };
